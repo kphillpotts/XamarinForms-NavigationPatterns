3fe6ff0fb6963fb29023750f2bd1904ae3cd793c
Kym Phillpotts <kphillpotts@gmail.com>
2015-02-22 23:24:35 +1100

    Added new switchboardbutton

---
+++ Switchboard/Switchboard/Switchboard/SwitchboardButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Switchboard
+{
+    public partial class SwitchboardButton : ContentView
+    {
+        public SwitchboardButton()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource Icon
+        {
+            get { return SwitchboardIcon.Source; }
+            set { SwitchboardIcon.Source = value; }
+        }
+
+        public string Label
+        {
+            get { return SwitchboardLabel.Text; }
+            set { SwitchboardLabel.Text = value; }
+        }
+
+
+    }
+}
